1f655704cc3a2af77a7119a1c4b948127d962a8b
shashank <17shashank17@gmail.com>
2019-11-02 17:42:29 +0530

    closest ambulance found

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var express=require('express')
 var mysql=require('mysql');
 
+const kmeans=require('node-kmeans');
+
+/*points=[[2,10],[2,5],[8,4],[5,8],[7,5],[6,4],[1,2],[4,9]]
+
+kmeans.clusterize(points,{k:3},(req,res,err)=>{
+    if(err) throw err;
+    else{
+        console.log('clusterised');
+        console.log('%o',res);
+    }
+})*/
+
 var app=express()
 
 var ejs=require('ejs');
@@ -9,6 +21,10 @@ var bodyparser=require('body-parser');
 urlencodedparser=bodyparser.urlencoded({extended: false});
 
 app.set('view engine','ejs')
+
+function findistance(point1,point2){
+    return Math.sqrt(Math.pow(point1[0]-point2[0],2)+ Math.pow(point1[1]-point2[1],2));
+}
 
 var db = mysql.createConnection({
     host     : 'localhost',
@@ -37,17 +53,31 @@ app.get('/',(req,res)=>{
 });
 
 app.post('/',urlencodedparser,(req,res)=>{
-    console.log(req.body);
-    console.log(`${req.body.fullname}`);
-    db.query(`insert into User values('${req.body.fullname}','${req.body.username}')`,(err,result)=>{
+
+    /*db.query(`insert into Patient_Record values('${req.body.x}','${req.body.y}','${req.body.problem}')`,(err,result)=>{
         if(err) throw err;
         else{
             console.log('insertion complete');
-            res.redirect('/');
+        }*/
+    db.query(`Select * from Ambulance where status='available'`,(err,results)=>{
+        console.log(results); 
+    
+        var points=[];
+        k=0;
+        min_distance=99999;
+        min_distance_index=-1;
+        for (i of results){
+            var distance=findistance([req.body.x,req.body.y],[i.x,i.y])
+            if (distance<min_distance){
+                min_distance=distance;
+                min_distance_index=k;
+            }
+            k+=1;
         }
+        console.log(results[min_distance_index].vehicle_no);
     });
 })
 
 app.listen(8080,()=>{
-    console.log('connected to the server')
+    console.log('connected to the server');
 })
