35474965fef4766d773d5b18f18c49558ebe328b
shashank <17shashank17@gmail.com>
2019-11-03 20:15:26 +0530

    ambulance found

--- index.js
+++ index.js
@@ -87,32 +87,27 @@ app.post('/',urlencodedparser,(req,res)=>{
         });
         var min=20000;
         var min_index=0;
-        db.query(`select Amb.vehicle_no from (select vehicle_no from Ambulance
+        db.query(`select x,y,vehicle_no from Ambulance_loc where vehicle_no in (select Amb.vehicle_no from (select vehicle_no from Ambulance
             where tools in (select Tools from Diseases where Disease='${req.body.problem}')) as Amb group by Amb.vehicle_no 
-            having count(Amb.vehicle_no)=(select count(Tools) from Diseases where Disease='${req.body.problem}')`,(err,results)=>{
-                console.log('Results from query:',results);
-                // var k=0;
-                for (i=0;i<results.length;i++){
-                    console.log('Individual Ambulance:',results[i]);
-                    db.query(`select x,y,vehicle_no from Ambulance_loc where vehicle_no = '${results[i].vehicle_no}' and status='available'`,(err,result)=>{
-                        if(err) throw err;
-                        else{ 
-                            console.log('In else');
-                            console.log('Ambulance Location',result[0].x,result[0].y)
-                            x=findistance([result[0].x,result[0].y],[req.body.x,req.body.y]);
-                            console.log('min_current',min, 'Current distance',x);
-                            if (min>x){
-                                min=x;
-                                min_index=k;
-                                console.log('updated index',min_index);
-                            }
+            having count(Amb.vehicle_no)=(select count(Tools) from Diseases where Disease='${req.body.problem}'))`,(err,results)=>{
+                if(err) throw err;
+                else{ 
+                    console.log(results);
+                    var min_dist=20000;
+                    var min_index=-1
+                    var k=0;
+                    for (i of results){
+                        x=findistance([i.x,i.y],[req.body.x,req.body.y])
+                        if(x<min){
+                            min=x;
+                            min_index=k;
                         }
-                    });
-                    k+=1;
+                        k+=1
+                    }
+                    console.log(results[min_index]);
+                    
                 }
-                console.log('Ans_min_index',min_index);
-                console.log('Ans',results[min_index]);
-            });
+        });
     }
 });
 
