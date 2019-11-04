e6de76f0ef924ad782fa02c4ff7e3a0e79a5daf1
shashank <17shashank17@gmail.com>
2019-11-04 15:57:29 +0530

    authorisation added

--- app.js
+++ app.js
@@ -1,8 +1,50 @@
 var express=require('express')
 
 var app=express();
+var ejslayout=require('express-ejs-layouts');
+
 
 app.set('view engine','ejs')
+app.use(ejslayout);
+
+const expressSession = require('express-session')
+const flash=require('connect-flash');
+const passport=require('passport');
+
+require('./config/passport')(passport);
+
+
+app.set('view engine','ejs');
+
+
+app.use(expressSession({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use((req,res,next) => {
+    res.locals.success_msg=req.flash('success_msg');
+    res.locals.error_msg=req.flash('error_msg');
+    res.locals.error=req.flash('error');
+    next();
+});
+
+var mongoose=require('mongoose');
+
+const db=require('./config/keys').MongoURI;
+
+
+
+mongoose.connect(db,{useUnifiedTopology:true,useNewUrlParser:true})
+    .then(() => console.log("MongoDB Connection Established"))
+    .catch(err => console.log(err));
+
 app.use('/',require('./Router/index.js'));
 app.use('/ambulance',require('./Router/ambulance.js'));
 
